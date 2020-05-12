bfd51d9a64d4a73ccbbb4cd7f11f516be19c78ef
Sanyam Sharma <sanyamsh20@gmail.com>
2020-05-12 15:19:00 +0530

    Update prefs.dart

--- lib/services/prefs.dart
+++ lib/services/prefs.dart
@@ -3,6 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:darkjokes/screens/choosePref.dart';
 import 'package:darkjokes/screens/home.dart';
 
+
 class Preferences{
   Widget showPrefs(){
     return FutureBuilder(
